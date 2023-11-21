ccb829d6312641b7dac859be28fbe5ecbc71a299
NurMuhammad1988 <nurikyorov88@gmail.com>
2023-11-21 17:07:56 +0500

    component props (table project)

--- react-Movie-Table-2/my-app/src/components/app/app.js
+++ react-Movie-Table-2/my-app/src/components/app/app.js
@@ -5,6 +5,15 @@ import AppFilter from "../app-filter/app-filter";
 import MovieList from "../movie-list/movie-list";
 import MoviesAddForm from "../movies-add-form/movies-add-form";
 const App = () => {
+
+    const data =[//dars uchun test server yani server eng yuqorida bo'lishi kerak yani shunda pastdagi content clasli divdagi filelargaham bu serverni chaqirsa bo'ladi bu server array yani massiv
+    {name:"Empire of Osman", viewers:988, favourite:false},
+    {name:"The great Seljuks", viewers:1091,favourite:true},
+    {name:"Ertugrul", viewers:789,favourite:false},
+    {name:"The great Seljuks Uyanish", viewers:977,favourite:true},
+    {name:"Salahuddin Quddus Fatihi", viewers:99977,favourite:true},
+]
+
     return (
         <div className="app font-monospace">
             <div className="content">
@@ -13,7 +22,8 @@ const App = () => {
                     <SearchPanel />
                     <AppFilter  />
                 </div>
-                <MovieList/>
+                <MovieList data={data}/>
+                {/* yani data yuqoridagi  serverdagi o'zgaruvchi serverni eng yuqoriga yozib  severni pastdagi hohlagan componentga shu tarzda chaqirib ishlatish mumkun */}
                 <MoviesAddForm/>
             </div>
         </div>
--- react-Movie-Table-2/my-app/src/components/movie-list/movie-list.js
+++ react-Movie-Table-2/my-app/src/components/movie-list/movie-list.js
@@ -1,14 +1,24 @@
 import "./movie-list.css";
 import MovieListItem from "../movie-list-item/movie-list-item";
 
-const MovieList = () => {
+const MovieList = ({ data }) => {
+    //bu data app.jsdagi server sifatida yozilgan data o'zgaruvchi
+    // console.log(props);
     return (
-        <div className="movie-list">
-            <MovieListItem />
-            <MovieListItem />
-            <MovieListItem />
-            <MovieListItem />
-        </div>
+        <ul className="movie-list">
+            {/* yani map orqali intirigatsa qilinib sikilga qo'yilib datadagi malumotlar movilistitemga jo'natildi 
+            desaham bo'ladi  */}
+            {data.map((item) => (
+                <MovieListItem name={item.name} viewers={item.viewers} favourite={item.favourite} />
+
+                // <MovieListItem {...item} />
+                //<MovieListItem {...item} /> manashu ko'rinishi object ochib interpalatsa orqali chaqirish deyiladi yani oson kam kod yoziladigan yo'li
+            ))}
+
+            {/* <MovieListItem name="Emire of Osman" viewers={811} />
+            <MovieListItem name="Ertugrul" viewers={890} />
+            <MovieListItem name="The great Seljuks" viewers={999} /> */}
+        </ul>
     );
 };
 
