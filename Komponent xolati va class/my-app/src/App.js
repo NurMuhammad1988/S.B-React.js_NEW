61afdeb4ab4b53da2786c716f960827efd50857a
NurMuhammad1988 <nurikyorov88@gmail.com>
2023-11-25 09:04:31 +0500

    setState method

--- Komponent xolati va class/my-app/src/App.js
+++ Komponent xolati va class/my-app/src/App.js
@@ -1,83 +1,156 @@
 import "./App.css";
+import { Component } from "react";
 
-// //properts
-// const User = (props) => {
-//     console.log(props);//propslar object ko'rinishida chiqadi
-//     return (
-//         <div
-//             style={{ backgroundColor: "black", margin: "20px", color: "wheat" }}
-//         >
-//             <h1>
-//                 Mening ismim - {props.firstName} ,sharifim - {props.lastName}{" "}
-//             </h1>
-//             <a href={props.link}>Ijtimoiy tarmog'i</a>
-//         </div>
-//     );
-// };
+////reactda hech qanday elementni yoki o'zgaruvchini to'g'ridan to'g'ri o'zgartirib bo'lmaydi
 
-// const App = () => {
-//     return (
-//         <div>
-//             <User firstName="Nur" lastName="Yorov" link="https://youtube.com" />
+////class componentlar reactda eskirgan hozirda asosan function componentlar ishlatiladi
+class User extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            //// state yani holat bu state reactda har doim object bo'ladi yani bu yerda oddiy objetc this.state yani aynan bu object ni o'zgartirish uchun this bilan o'zgaradi // yani reactda o'zgaruvchini o'zgartirish uchun state bilan object ko'rinishida ochib o'zgartirish mumkun
 
-//             <User
-//                 firstName="Firdavs"
-//                 lastName="Yorov"
-//                 link="https://facebook.com"
-//             />
+            counter: 0,
+        };
+    }
 
-//             <User
-//                 firstName="Muso"
-//                 lastName="Yorov"
-//                 link="https://twitter  .com"
-//             />
-//         </div>
-//     );
-// };
+    clickIncrement = () => {
+        this.setState((prevState) => ({//prevState  metodi bu oldingi holati muhum bo'lgan holatlarsa ishlatiladi yani raqamlarni o'tqizib yubormaydi serverdan malumot sekin kelgan paytdaham user tez bosganda malumotlarni o'tqizib yubormaydi
+            counter: prevState.counter + 1,
+        }));
+    };
 
-// export default App;
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    clickDecrement = () => {
+        this.setState((prevState) => ({
+            counter: prevState.counter - 1,
+        }));
+    };
 
-//// Disruption
+    clickRestart = () => {
+        this.setState({
+            //prevState endi bu yerda shart emas
+            counter: 0,
+        });
+    };
 
-const User = ({ firstName, lastName, link }) => {
-    //agar malumotlar ko'p bo'lsa tashqarida props bilan qilingan maqul lekin 5 10 tagacha shu holatda disturubsatsa qilingani yahshi
-    return (
-        <div className="propss1"
-           
-        >
-            {/*  style={{ backgroundColor: "black", margin: "20px", color: "wheat" }} divga htmlni o'zida style berish */}
-            <h1>
-                Mening ismim - {firstName.name} ,sharifim - {lastName()}
-                {/*jsxni ichiga object berib bo'lmaydi yani bu joyda firstname object bu objectni o'qitish uchun manabunday yoziladi =>{firstName.name} va manabunday qilib chaqiriladi =>{lastName()} yani bu joyda last name funcsiya hissoblanadi*/}
-            </h1>
-            <a href={link}>Youtube kanalim</a>
-        </div>
-    );
-};
+    render() {
+        //classda render metodi bilan jsxni render qilish kerak
+        const { firstName, lastName, link } = this.props;
+        // console.log(this.props);
+        return (
+            <div className="w-50 mx-auto">
+                <div className="border p-3 mt-5 ">
+                    <h4 className="text-warning">
+                        Mening ismim - {firstName}, sharifim - {lastName}
+                    </h4>
+                    <a href={link}>Youtube kanalim</a>
+                    <div className="mt-3">
+                        <button
+                            onClick={this.clickIncrement}
+                            className="btn btn-success"
+                        >
+                            Increment
+                        </button>
+
+                        <button
+                            onClick={this.clickDecrement}
+                            className="btn btn-danger mx-2"
+                        >
+                            Decrement
+                        </button>
+
+                        <button
+                            onClick={this.clickRestart}
+                            className="btn btn-success"
+                        >
+                            Restart
+                        </button>
+                        <p className="text-warning mt-3">
+                            {this.state.counter}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
 
 const App = () => {
     return (
-        <div className="propss2">
-            <User
-                firstName={{ name: "Nur" }}
-                lastName={() => "Yorov"} //last name bu joyda funksiya (manashuni ichiga yozilmadi chunku bu qavusni ichida hech narsa ko'rinmagani bilan aslida ichida default holatida return bo'ladi ) //yani agar kerak bo'lsa bu joyda lastnameham firstnameham  funksiya qilib yozilib jsxda manabunday holatda {lastName ()} chaqirilib ishlatiladi (jsx codlarga qaralsin)
-                link="https://youtube.com"
-            />
+        <div>
+            <User firstName="Nur" lastName="Yorov" link="youtube.com" />
 
-            <User
-                firstName={{ name: "Firdavs" }}
-                lastName={() => "Yorov"}
-                link="https://facebook.com"
-            />
-
-            <User
-                firstName={{ name: "Muso" }}
-                lastName={() => "Yorov"}
-                link="https://twitter.com"
-            />
+            <User firstName="Nur2" lastName="Yorov2" link="youtube.com" />
         </div>
     );
 };
 
 export default App;
+
+// import "./App.css";
+// import { Component } from "react";
+
+// ////reactda hech qanday elementni yoki o'zgaruvchini to'g'ridan to'g'ri o'zgartirib bo'lmaydi
+
+// ////class componentlar reactda eskirgan hozirda asosan function componentlar ishlatiladi
+// class User extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             //// state yani holat bu state reactda har doim object bo'ladi yani bu yerda oddiy objetc this.state yani aynan bu object ni o'zgartirish uchun this bilan o'zgaradi // yani reactda o'zgaruvchini o'zgartirish uchun state bilan object ko'rinishida ochib o'zgartirish mumkun
+
+//             counter: 10,
+//         };
+//     }
+
+//     clickHandler = () => {
+//         this.setState((prevState) => ({
+//             counter: prevState.counter + 1,
+//         }));
+//     };
+
+//     // clickHandler = () => {
+//     //     this.setState({
+//     //         counter: this.state.counter + 1,
+//     //     });
+//     // };
+
+//     render() {
+//         //classda render metodi bilan jsxni render qilish kerak
+//         const { firstName, lastName, link } = this.props;
+//         // console.log(this.props);
+//         return (
+//             <div className="w-50 mx-auto">
+//                 <div className="border p-3 mt-5 ">
+//                     <h4 className="text-warning">
+//                         Mening ismim - {firstName}, sharifim - {lastName}
+//                     </h4>
+//                     <a href={link}>Youtube kanalim</a>
+//                     <div className="mt-3">
+//                         <button
+//                             onClick={this.clickHandler}
+//                             className="btn btn-success"
+//                         >
+//                             Click
+//                         </button>
+//                         <p className="text-warning mt-3">
+//                             {this.state.counter}
+//                         </p>
+//                     </div>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+
+// const App = () => {
+//     return (
+//         <div>
+//             <User firstName="Nur" lastName="Yorov" link="youtube.com" />
+
+//             <User firstName="Nur2" lastName="Yorov2" link="youtube.com" />
+//         </div>
+//     );
+// };
+
+// export default App;
