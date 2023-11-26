84497036d2c7f47ed47ad264868b5181e4e3d01e
NurMuhammad1988 <nurikyorov88@gmail.com>
2023-11-26 22:47:45 +0500

    + class component with movie list project

--- react-Movie-Table/my-app/src/components/movie-list-item/movie-list-item.js
+++ react-Movie-Table/my-app/src/components/movie-list-item/movie-list-item.js
@@ -1,39 +1,98 @@
+import { Component } from "react";
 import "./movie-list-item.css";
 
-const MovieListItem = ({ name, viewers, favourite }) => {
-    // console.log(favourite);
+class MovieListItem extends Component {//bu dars class component orqali qilingani huddi shuni san function component orqaliham bajarib qo'y
+    constructor(props) {
+        super(props);
+        this.state = { favourite: false, like: false };
+    }
 
-    //// let classNames=`list-group-item d-flex justify-content-between`
-    //// if(favourite){
-    // //     classNames+= " favourite"
-    // // }
-    ////  bu ikkinchi yo'li yani o'zgaruvchi yaratib qilish yo'li lekin birinchi yani manabu yo'li=> oson va tushunarli  <li className={`list-group-item d-flex justify-content-between ${favourite && "favourite"}`}> bu jsxni ichida yozilgan pastda
+    onFavourite = () => {
+        this.setState(({ favourite }) => ({
+            favourite: !favourite,
+        }));
+    };
 
-    return (
-        // <li className={classNames}>//bu ikkinchi yo'lini jsx ichida yozilishi
-        <li
-            className={`list-group-item d-flex justify-content-between ${
-                favourite && "favourite"
-            }`}
-        >
-            {/* tepadagi birinchi yo'li */}
-            <span className="list-group-item-label">{name}</span>
-            <input
-                type="number"
-                className="list-group-item-input"
-                defaultValue={viewers}
-            />
-            <div className="d-flex justify-content-center .align-items-center">
-                <button type="button" className="btn-cookie btn-sm">
-                    <i className="fas fa-cookie"></i>
-                </button>
-                <button type="button" className="btn-trash btn.sm">
-                    <i className="fas fa-trash"></i>
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
-    );
-};
+    onLike =()=>{
+        this.setState(({like}) =>({
+            like:!like
+        }))
+    }
+
+    render() {
+        const { name, viewers } = this.props;
+        const { favourite, like } = this.state;
+
+        return (
+            // <li className={classNames}>//bu ikkinchi yo'lini jsx ichida yozilishi
+            <li
+                className={`list-group-item d-flex justify-content-between ${
+                    favourite && "favourite"
+                } ${like && "like"}`}
+            >
+                {/* tepadagi birinchi yo'li */}
+                <span onClick={this.onLike} className="list-group-item-label">{name}</span>
+                <input
+                    type="number"
+                    className="list-group-item-input"
+                    defaultValue={viewers}
+                />
+                <div className="d-flex justify-content-center .align-items-center">
+                    <button
+                        type="button"
+                        className="btn-cookie btn-sm"
+                        onClick={this.onFavourite}
+                    >
+                        <i className="fas fa-cookie"></i>
+                    </button>
+                    <button type="button" className="btn-trash btn.sm">
+                        <i className="fas fa-trash"></i>
+                    </button>
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>
+        );
+    }
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// const MovieListItem = ({ name, viewers, favourite }) => {
+//     // console.log(favourite);
+
+//     //// let classNames=`list-group-item d-flex justify-content-between`
+//     //// if(favourite){
+//     // //     classNames+= " favourite"
+//     // // }
+//     ////  bu ikkinchi yo'li yani o'zgaruvchi yaratib qilish yo'li lekin birinchi yani manabu yo'li=> oson va tushunarli  <li className={`list-group-item d-flex justify-content-between ${favourite && "favourite"}`}> bu jsxni ichida yozilgan pastda
+
+//     return (
+//         // <li className={classNames}>//bu ikkinchi yo'lini jsx ichida yozilishi
+//         <li
+//             className={`list-group-item d-flex justify-content-between ${
+//                 favourite && "favourite"
+//             }`}
+//         >
+//             {/* tepadagi birinchi yo'li */}
+//             <span className="list-group-item-label">{name}</span>
+//             <input
+//                 type="number"
+//                 className="list-group-item-input"
+//                 defaultValue={viewers}
+//             />
+//             <div className="d-flex justify-content-center .align-items-center">
+//                 <button type="button" className="btn-cookie btn-sm">
+//                     <i className="fas fa-cookie"></i>
+//                 </button>
+//                 <button type="button" className="btn-trash btn.sm">
+//                     <i className="fas fa-trash"></i>
+//                 </button>
+//                 <i className="fas fa-star"></i>
+//             </div>
+//         </li>
+//     );
+// };
 
 export default MovieListItem;
