97d3ae32c6a17d341d337edeab16c63a07eb9601
NurMuhammad1988 <nurikyorov88@gmail.com>
2023-11-28 00:20:52 +0500

    Mutable va immutable va shaxsiy xodisalar

--- react-Movie-Table/my-app/src/components/app/app.js
+++ react-Movie-Table/my-app/src/components/app/app.js
@@ -1,43 +1,89 @@
 import "./app.css";
+import { Component } from "react";
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
 import AppFilter from "../app-filter/app-filter";
 import MovieList from "../movie-list/movie-list";
 import MoviesAddForm from "../movies-add-form/movies-add-form";
-const App = () => {
-    
-    const data = [
-        //dars uchun test server yani server eng yuqorida bo'lishi kerak yani shunda pastdagi content class li divdagi filelargaham bu serverni chaqirsa bo'ladi bu server array yani massiv shuni uchun tepada turish kerak yani kod o'qish qoidasiga muofiq
-        { name: "Empire of Osman", viewers: 988, favourite: false, id: 1 },
-        { name: "The great Seljuks", viewers: 1091, favourite: true, id: 2 },
-        { name: "Ertugrul", viewers: 789, favourite: false, id: 3 },
-        {
-            name: "The great Seljuks Uyanish",
-            viewers: 977,
-            favourite: true,
-            id: 4,
-        },
-        {
-            name: "Salahuddin Quddus Fatihi",
-            viewers: 99977,
-            favourite: true,    
-            id: 5,
-        },
-    ];
 
-    return (
-        <div className="app font-monospace">{/*yani bu joyda font-monospase bu shrift chunku bu htmlda cdn.js orqali linki ulab qo'yilgan shu uchun font awesomeni classlari chaqirilganda yani clasnamega yozilganda ishlayveradi */}
-            <div className="content"> {/*bu content clasname asosiy content yani saytni asosi hissobladi yani bu asosiy app.jsda bo'ladi */}
-                <AppInfo />
-                <div className="search-panel">
-                    <SearchPanel />
-                    <AppFilter />
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                //dars uchun test server yani server eng yuqorida bo'lishi kerak yani shunda pastdagi content class li divdagi filelargaham bu serverni chaqirsa bo'ladi bu server array yani massiv shuni uchun tepada turish kerak yani kod o'qish qoidasiga muofiq
+                {
+                    name: "Empire of Osman",
+                    viewers: 988,
+                    favourite: false,
+                    id: 1,
+                },
+                {
+                    name: "The great Seljuks",
+                    viewers: 1091,
+                    favourite: true,
+                    id: 2,
+                },
+                {
+                    name: "Kurulis Ertugrul",
+                    viewers: 789,
+                    favourite: false,
+                    id: 3,
+                },
+                {
+                    name: "The great Seljuks Uyanish",
+                    viewers: 977,
+                    favourite: true,
+                    id: 4,
+                },
+                {
+                    name: "Salahuddin Quddus Fatihi",
+                    viewers: 99977,
+                    favourite: true,
+                    id: 5,
+                },
+            ],
+        };
+    }
+
+  
+
+    onDelete = (id) => {
+        // console.log(id);
+        this.setState(({ data }) => ({
+            data: data.filter((c) => c.id !== id),
+
+            //const index = data.findIndex((c) => c.id === id);//mutable yani o'zgaruvchan
+            // data.splice(index,1)//mutable yani o'zgaruvchan
+
+            // const newArray = data.filter((c) => c.id !== id);//imutable yani o'zgarmas
+
+            // return {
+            //     data: data.filter((c) => c.id !== id),
+            // };
+        }));
+    };
+
+    render() {
+        const { data } = this.state;
+
+        return (
+            <div className="app font-monospace">
+                {/*yani bu joyda font-monospase bu shrift chunku bu htmlda cdn.js orqali linki ulab qo'yilgan shu uchun font awesomeni classlari chaqirilganda yani clasnamega yozilganda ishlayveradi */}
+                <div className="content">
+                    {" "}
+                    {/*bu content clasname asosiy content yani saytni asosi hissobladi yani bu asosiy app.jsda bo'ladi */}
+                    <AppInfo />
+                    <div className="search-panel">
+                        <SearchPanel />
+                        <AppFilter />
+                    </div>
+                    <MovieList data={data} onDelete={this.onDelete} />
+                    {/* yani data yuqoridagi  serverdagi o'zgaruvchi serverni eng yuqoriga yozib  severni pastdagi hohlagan componentga shu tarzda chaqirib ishlatish mumkun */}
+                    <MoviesAddForm />
                 </div>
-                <MovieList data={data} />
-                {/* yani data yuqoridagi  serverdagi o'zgaruvchi serverni eng yuqoriga yozib  severni pastdagi hohlagan componentga shu tarzda chaqirib ishlatish mumkun */}
-                <MoviesAddForm />
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
 export default App;
--- react-Movie-Table/my-app/src/components/movie-list/movie-list.js
+++ react-Movie-Table/my-app/src/components/movie-list/movie-list.js
@@ -1,7 +1,7 @@
 import "./movie-list.css";
 import MovieListItem from "../movie-list-item/movie-list-item";
 
-const MovieList = ({ data }) => {
+const MovieList = ({ data, onDelete }) => {
     //bu data app.jsdagi server sifatida yozilgan data o'zgaruvchi
     // console.log(props);
     return (
@@ -11,10 +11,11 @@ const MovieList = ({ data }) => {
             {/* jsx ichida massivlarni intirigatsa qilsaham bo'ladi */}
             {data.map((item) => (
                 <MovieListItem
-                    key={item.id}//UNIQUE YANI UNIKAL YANI TAKRORLANMAS id berilmasa react har yangi component qo'shilganda hammasini qaytadan quradi yani 2 ta component bor bo'lsayu 3 chisi qo'shilsa shunda react oldin yasalgan 2ta componentniham qaytadan yasaydi agar id bilan qo'yilsa faqat yangi qo'shilgan idgina qo'shiladi va yana component serverdan massiv holatda qaytganda 0 dan boshlanadigan tartib raqamiham o'zgarmaydi yani ichidan birirtasi udalit qilinsa faqat udalit qilingan component udalit boladi va tartib raqam o'zgarmeydi
+                    key={item.id} //UNIQUE YANI UNIKAL YANI TAKRORLANMAS id berilmasa react har yangi component qo'shilganda hammasini qaytadan quradi yani 2 ta component bor bo'lsayu 3 chisi qo'shilsa shunda react oldin yasalgan 2ta componentniham qaytadan yasaydi agar id bilan qo'yilsa faqat yangi qo'shilgan idgina qo'shiladi va yana component serverdan massiv holatda qaytganda 0 dan boshlanadigan tartib raqamiham o'zgarmaydi yani ichidan birirtasi udalit qilinsa faqat udalit qilingan component udalit boladi va tartib raqam o'zgarmeydi
                     name={item.name}
                     viewers={item.viewers}
                     favourite={item.favourite}
+                    onDelete={() => onDelete(item.id)}
                 />
 
                 //// <MovieListItem {...item} />
