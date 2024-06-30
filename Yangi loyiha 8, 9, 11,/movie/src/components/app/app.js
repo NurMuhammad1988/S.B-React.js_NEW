2afb83572f2e56dfd49d740c95aade50061317dc
NurMuhammad1988 <nurikyorov88@gmail.com>
2024-06-30 08:57:03 +0500

    dsdsds

--- Yangi loyiha 8, 9, 11,/movie/src/components/app/app.js
+++ Yangi loyiha 8, 9, 11,/movie/src/components/app/app.js
@@ -6,6 +6,13 @@ import MovieList from "../movie-list/movie-list";
 import MoviesAddForm from "../movies-add-form/movies-add-form";
 
 const App = () => {
+    const data = [
+        //datani yani malumotlar bazasini Appda eng yuqoriga qo'yish kerak //bu local server yani real loyihalarda datalar serverda turadi darsda server yo'qligi sababli serverni qo'lada shunday yozib turuldi server doim hamma componentlardan yuqorida yozilishi kerak chunki agar data bu holatda Appni ichida ishlatilganda yuqorida yozlishi yani turishi kerak chunki datalar Appni ichida hohlagan componentda chaqirilishi mumkun shu sabab server tepada turishi kerak shudna pastda hohlagan componentda serverdan malumotlar chaqirilsa kod o'qish tartibi bo'yihca server kodlar birinchi o'qilib tayyor qilinib pastdagi comonentlarda ishlaydi (boradi) masalan user SearchPaneldagi inputdan malumot izlasa serverdan keladi server tepada tursa koddagi vazifalar bajarilib bo'linib SearchPnaeldan user malumot izlaganda malumotlar tayyor bo'lib turadi qolgan componentlardaham shunga o'hshash vazifalar bor //serverdan malumotlar massivni ichida object bo'lib keladi
+        { name: "Emire of Usman", viewers: 988 },
+        { name: "Ertugrul", viewers: 791 },
+        { name: "Omar", viewers: 1091 },
+    ];
+
     return (
         <div className="app font-monospace">
             <div className="content">
@@ -15,7 +22,7 @@ const App = () => {
                     <SearchPanel />
                     <AppFilter />
                 </div>
-                <MovieList />
+                <MovieList data={data} />
                 <MoviesAddForm />
             </div>
         </div>
--- Yangi loyiha 8, 9, 11,/movie/src/components/movie-list-item/movie-list-item.js
+++ Yangi loyiha 8, 9, 11,/movie/src/components/movie-list-item/movie-list-item.js
@@ -1,19 +1,21 @@
 import "./movie-list-item.css";
 
-const MovieListItem = () => {
+const MovieListItem = ({ name, viewers }) => {
+    // console.log(props);
     return (
         <li className="list-group-item d-flex justify-content-between">
+            <span className="list-group-item-label">{name}</span>
 
-            <span className="list-group-item-label">Emire of Usman</span>
+            {/*  endi namelar dynamic tarzda movielist.js filedan kelepti stylelari bilan birga */}
 
             <input
                 type="number"
                 className="list-group-item-input"
-                defaultValue="989"
+                defaultValue={viewers}
+                // endi viewerslar dynamic tarzda movielist.js filedan kelepti stylelari bilan birga
             />
 
             <div className="d-flex justify-content-center align-items-center">
-
                 <button type="button" className="btn-cookie btn-sm">
                     <i className="fas fa-cookie"></i>
                 </button>
