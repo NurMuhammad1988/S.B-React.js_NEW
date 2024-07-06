d19e412230a111f5ab27f13e72001abedbf3c277
NurMuhammad1988 <nurikyorov88@gmail.com>
2024-07-06 08:45:51 +0500

    dddsssddd

--- Yangi loyiha 8, 9, 11, 12, 15, 16, 17, 18, 20, 21, 22/movie/src/components/app-info/app-info.js
+++ Yangi loyiha 8, 9, 11, 12, 15, 16, 17, 18, 20, 21, 22/movie/src/components/app-info/app-info.js
@@ -1,12 +1,16 @@
-import "./app-info.css"
+// import "./app-info.css";
+import "./app-info.scss";//scssni chaqirish
 
-const AppInfo = ({allMoviesCount, favoriteMoviesCount}) => (
-  <div className="app-info">
-    {/* inlise style in;ine stylelar berganda bootstrapni stylelari o'chirib turildi chunki inline stylelar yakka o'zi yozilishi kerak ekan yani pxni react aftamatik tarzda qo'yadi>>>style={{ fontSize: 90 }} inline stylelarda lekin pxdan boshqalarda masalan remda rem yozilishi kerak>>>style={{fontSize: "30rem"}} */}
-    <p className="fs-3 text-uppercase" style={{ fontSize: 90 }}>Barcha kinolar soni: {allMoviesCount}</p>
-    <p className="fs-4 text-uppercase">Sevimli Filmlar:{favoriteMoviesCount} </p>
 
-  </div>
-)
-22. Inline style va SCSS darsi  qoldi
-export default AppInfo
+const AppInfo = ({ allMoviesCount, favoriteMoviesCount }) => (
+    <div className="app-info">
+        {/* inlise style in;ine stylelar berganda bootstrapni stylelari o'chirib turildi chunki inline stylelar yakka o'zi yozilishi kerak ekan yani pxni react aftamatik tarzda qo'yadi>>>style={{ fontSize: 90 }} inline stylelarda lekin pxdan boshqalarda masalan remda rem yozilishi kerak>>>style={{fontSize: "30rem"}} inline styleda animatsiyalarniham qilish mumkun>>>style={{ fontSize:"1rem" , fontWeight: "bold", animation: "all", WebkitAnimation: "all" } } */}
+        <p className="fs-3 text-uppercase">
+            Barcha kinolar soni: {allMoviesCount}
+        </p>
+        <p className="fs-4 text-uppercase">
+            Sevimli Filmlar:{favoriteMoviesCount}{" "}
+        </p>
+    </div>
+);
+export default AppInfo;
