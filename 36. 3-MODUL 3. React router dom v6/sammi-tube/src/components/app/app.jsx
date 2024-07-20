55aad7af229d0cd60c136119574d1c74200eb5c2
NurMuhammad1988 <nurikyorov88@gmail.com>
2024-07-21 00:40:17 +0500

    ddddddd

--- 36. 3-MODUL 3. React router dom v6/sammi-tube/src/components/app/app.jsx
+++ 36. 3-MODUL 3. React router dom v6/sammi-tube/src/components/app/app.jsx
@@ -1,16 +1,17 @@
-import { Stack, Button, Container } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Main, Channel } from "../";
 
 const App = () => {
     return (
-        <Container>
-    
-            <Stack spacing={2} direction="row">
-             
-                <Button variant="text">Text</Button>
-                <Button variant="contained">Contained</Button>
-                <Button variant="outlined">Outlined</Button>
-            </Stack>
-        </Container>
+        <Box>
+            {/* box bu material uidan chaqirilgan divga o'hshagan narsa yani ona div lekin bu boxda material ui bilan ishlash uchun kerak bo'ladigan atributelari bor */}
+            <Routes>
+                {/* ROUTESDA doim route componentlar bo'ladi route path atributini qabul qiladi shu sabab ROUTESNI ICHIDA ROUTE BO'LISHI KERAK  */}
+                <Route path="/" element={<Main />} />
+                <Route path="/channel" element={<Channel />} />
+            </Routes>
+        </Box>
     );
 };
 
