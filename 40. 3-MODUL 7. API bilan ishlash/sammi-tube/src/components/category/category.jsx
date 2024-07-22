f9aa16cdc3567c7619f5156dbb41ac51021b7aea
NurMuhammad1988 <nurikyorov88@gmail.com>
2024-07-22 16:53:09 +0500

    dddd

--- 40. 3-MODUL 7. API bilan ishlash/sammi-tube/src/components/category/category.jsx
+++ 40. 3-MODUL 7. API bilan ishlash/sammi-tube/src/components/category/category.jsx
@@ -2,7 +2,8 @@ import { Stack } from "@mui/material";
 import { category } from "../../constants";
 import { colors } from "../../constants/colors";
 
-const Category = ({ selectedCatigoryHandler, selectedCategory }) => {
+////                 usestatedan keletgan ikita massivlar
+const Category = ({ selectedCategoryHandler, selectedCategory }) => {
     return (
         <Stack direction={"row"} sx={{ margin: "auto", display: "flex" }}>
             {/* bu material uini stack componentiga ushbu style berilgan edi>>>sx={{overflowX: "scroll" }} yani agar category function componentni elementlari uzun bo'lib x o'qi bo'yicha ekranga sig'may qolsa scroll bo'lsin chapga o'ngga yursin ekranga sig'may qolgan elementlarham scroll sabab ko'rinsin deyilganda (yani darsda ustozni manitorida)lekin hozirda bu dars qilinayotgan manitor ancha katta bo'lganligi sababli bu styleni o'chirib qo'ydim chunki scroll hunuk ko'rinayapti buni o'rniga>>>sx={{margin: "auto"}} style qo'yildi sabab category component elementlari bilan manitorni o'rtasida turishi uchun agar kichkinaroq manitorda sayt ko'rilsa ustoz yozgan stylelarni yozib qo'yish kerak ???????????LEKIN BU HOLATDA SAYT RESPONSE BO'LMAYAPTI YANI BOSHQA QURILMALARGA MOSLASHMAYAPTI?????????????????   */}
@@ -14,9 +15,12 @@ const Category = ({ selectedCatigoryHandler, selectedCategory }) => {
                         borderRadius: "0",
                         background:
                             item.name === selectedCategory && colors.secondary,
+                            // yani setstateni birinchi qiymatini default holati selectedcategoryga teng
                         color: item.name === selectedCategory && "#fff",
                     }}
-                    onClick={() => selectedCatigoryHandler(item.name)}
+                    // agar index.jsdan map qilingan category massivini item.namelarini   backgroundi: yani birinchi string malumotlarini backgroundi qattiy teng bo'lsa  selectedCategoryga yani main componentda yozilgan classga activ bo'lib qattiy teng bo'lsa colorsdagi secondariyni ishlat yokida default holatda qolsin yani default holati oq//////agar color: agar item.namelarni colori qattiy teng bo'lsa selectedCategory yani activ classga item.namelarni rangini oq qilib qo'y default holatda esa qora
+                    onClick={() => selectedCategoryHandler(item.name)}
+                    // default holatda Newda turipti yani map qilingan category massivini birinchi namesi "New" string default holatda turipti qolganlariga bosganda esa activ class bosilgani bo'ladi
                 >
                     <span
                         style={{
@@ -29,9 +33,12 @@ const Category = ({ selectedCatigoryHandler, selectedCategory }) => {
                     >
                         {item.icon}
                     </span>
+                    {/* bu birinchi spanda map qilingan categoryni iconlar bor bunga agar item icon selectedCategoryga yani activ classga teng bo'lsa rangini  oq qil yokida colorsni ichidagi secondary rangidada qotib tur deyilgan desaham bo'ladi */}
+
                     <span style={{ opacity: "1", marginRight: "20px" }}>
                         {item.name}
                     </span>
+                    {/* yani bu item.namelarni default stylelari yuqoridagi if elslar esa defaultmas holatlar yani activ class uchun */}
                 </button>
             ))}
         </Stack>
--- 40. 3-MODUL 7. API bilan ishlash/sammi-tube/src/components/main/main.jsx
+++ 40. 3-MODUL 7. API bilan ishlash/sammi-tube/src/components/main/main.jsx
@@ -1,26 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Box, Container, Stack, Typography } from "@mui/material";
 import { colors } from "../../constants/colors";
-import { Category } from "../";
+import { Category, Videos } from "../";
+import { ApiService } from "../../service/api.service";
+// ApiService bu constructor
 
 const Main = () => {
     const [selectedCategory, setSelectedCategory] = useState("New");
+    // default holatda Newda turipti yani Category componentda map qilingan category massivini birinchi namesi New string default holatda turipti
 
-    const selectedCatigoryHandler = (catecory) => setSelectedCategory(catecory)
+    // 30 : 19 chi minutda qoldi
+
+    const selectedCategoryHandler = (catecory) => setSelectedCategory(catecory);
+
+    //bu holatda reactdan usestate chaqirilib birinchi bo'sh parametriga selectedCategory nomli o'zgaruvchi massiv yaratib olindi va shu selectedCategory o'zgargandagi qiymatlari setSelectedCategory yangi o'zgaruvchiga default holati string new qilib qo'yildi va selectedCategoryHandler nomli o'zgaruvchi yaratilib usestateni yangi o'zgargan setSelectedCategory massiv objecti parametrida category texti bilan chaqirildi va bu main componentida chaqirilgan catecory componentiga ichida setSelectedCategory o'zgaruvchan massivi bor selectedCategoryHandler o'zgaruvchi chaqirib qo'yildi sababi selectedCategoryHandlerni ichida setSelectedCategory bor va bu setSelectedCategory parametr qabul qiladi va yana usestateni birinchi qiymati new string bo'lgan selectedCategory o'zgaruvchiham chaqirildi endi category componentida selectedCategoryda default new string bor va selectedCategoryHandlerda o'zgaradigan setSelectedCategory bor endi Boxni ichiga container va Typography ichiga selectedCategory yani usestateni birinchi ichida faqat new string bor objecti chaqirildi shunda category componentida birinchi default new string turadi
+
+    // console.log(process.env.REACT_APP_PUPLIC_KEY);
+    // .evn va api.service.js filedagi server yani api kalitlar hafsizligini tekshirib ko'rish uchun yozildi
+
+    useEffect(() => {
+        // bu fetching functsiya api service fileni ichida yozilgan url qabul qiladigan axios bilan ishleydigan ApiService asyng funksiyani ichki funksiyasi//.then promise!!! 
+        ApiService.fetching("search").then(data => console.log(data))
+
+    }, []);
 
     return (
         <Stack>
-            
-            <Category selectedCatigoryHandler={selectedCatigoryHandler} selectedCategory={selectedCategory}/>
+            <Category
+                selectedCategoryHandler={selectedCategoryHandler}
+                selectedCategory={selectedCategory}
+            />
             <Box p={2} sx={{ height: "90vh" }}>
                 <Container maxWidth={"90%"}>
                     <Typography variant={"h4"} fontWeight={"bold"} mb={2}>
-                       
-                        {selectedCategory}{" "}
-                        <span style={{ color: colors.secondary }}>videos</span>
-                        
+                        {selectedCategory}
+                        <span style={{ color: colors.secondary }}> videos</span>
                     </Typography>
-                    Videos
+                    <Videos />
                 </Container>
             </Box>
         </Stack>
