3b1d67f07a419c1ea6efa278a02167b9c2cf9974
NurMuhammad1988 <nurikyorov88@gmail.com>
2024-07-25 02:13:25 +0500

    fdfdfd

--- 43. 3-MODUL 10. Search page/sammi-tube/src/components/video-card/video-card.jsx
+++ 43. 3-MODUL 10. Search page/sammi-tube/src/components/video-card/video-card.jsx
@@ -15,10 +15,10 @@ const VideoCard = ({ video }) => {
     console.log(video);
     return (
         <Card
-            
+        
             sx={{
                 width: { xs: "100%", sm: "360px", md: "320px" },
-                // yani xsda yani 0dan 600pxgacha hardni o'lchami 100% v smda yani 600pxdan yuqorida 900pxgacha har bir cardni o'lchami 360px va mdda 900pxdan yuqorida 1200pxgacha 320px
+              
                 boxShadow: "none",
                 borderRadius: 0,
             }}
@@ -30,9 +30,7 @@ const VideoCard = ({ video }) => {
             />
 
             <CardContent
-                //breakpoints ranglar>> //sx={{ background:{xs:"black", sm:"red", md:"yellow", lg:"aqua", xl:"brown"}}
-                // material uida response qilish (breakpoints) yani xs 0pxdan hissobleydi qancha pixelgacha ekanligini aytish kerak sm esa 600pxldan boshlanadi shunda background 0px dan 600pxgacha qora 600pxdan yuqorisi agar buyruq md, lg, xl qilib berilmasa har qanday size uchun qizil lekin bundan smdan keyingi sizlargaham alohida buyruq berib har qanday sizeda har qanday sharoitda turishini taminlash mumkun
-                //////yani aynan bu holatda xsda:bo'lsa 0pxdan 600pxgacha black va smda:bo'lsa 600pxdan  900pxgacha red va mdda:bo'lsa 900pxdan 1200pxgacha yellow va lgda:bo'lsa 1200pxdan 1536pxgacha aqua va xl:da bo'lsa 1536pxdan yuqorisida qanday o'lchamli monitor bo'lishidan qattiy nazar brown
+                
                 sx={{
                     background: colors.primary,
                     height: "200px",
